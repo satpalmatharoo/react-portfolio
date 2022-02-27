a5924f114d88a76626e56886f8836f918cb37696
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-02-27 02:01:57 +0000

    create and add styling to navbar

---
+++ reactportfolio/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const navbar = () => (
+    <navbar>
+        <li><a>aboutme</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>Contact</a></li>
+    </navbar>
+
+    
+);
+
+export default Navbar;
