f06aa2cece37db4d1fe94e0835eace1f0d6d6b5e
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-02-27 02:02:17 +0000

    import components

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+ import './App.css';
+ //import components
+ import Aboutme from './components/pages/Aboutme';
+ import Portfolio from './components/pages/Portfolio';
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <h1> like such a big smurfy nerd</h1> */}
+    <div className="home">
+    <Aboutme />
+    <Portfolio/>
+    </div>
     </div>
   );
 }
+
+
+
+
+
+//     <div className="App">
+//       <header className="App-header"> ..  
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
