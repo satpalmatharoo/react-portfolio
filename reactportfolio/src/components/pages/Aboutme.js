312ef34d38e441cbdcbb9f6379f734edbded7769
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-02-27 02:02:45 +0000

    build components folder

---
+++ reactportfolio/src/components/pages/Aboutme.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Aboutme = () => (
+    <aboutme>
+        <p>
+        Positive and energetic outlook which is probably why I love problem solving. Currently enrolled 
+        on a full stack web developer course accredited by theUniveristy of Birmingham, 
+        experienced in HTML, CSS, JavaScript and API Systems
+        </p>
+
+        <p>
+        Adaptable and fun
+        </p>
+    </aboutme>
+
+    
+);
+
+export default Aboutme;
---
+++ reactportfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = () => (
+    <section>
+       <h1>Portfolio of Works</h1>
+    </section>
+
+    
+);
+
+export default Portfolio;
