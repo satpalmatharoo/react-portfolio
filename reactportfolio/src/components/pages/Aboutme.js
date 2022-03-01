124ab14003b6acd2c93abb84910a69805364ec14
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-03-01 22:09:54 +0000

    add contact details

--- reactportfolio/src/components/pages/Aboutme.js
+++ reactportfolio/src/components/pages/Aboutme.js
@@ -5,15 +5,19 @@ const Aboutme = () => (
 
         <div>
             <h1 className="header">
-                <span> About Mehh!</span>
+                <span> About Satpal</span>
             </h1>
             <div id> "about-header"</div>
 
         
         <p>
-        Positive and energetic outlook which is probably why I love problem solving. Currently enrolled 
-        on a full stack web developer course accredited by theUniveristy of Birmingham, 
+        Positive and energetic outlook which is probably why I love to problem solve. Currently enrolled 
+        on a full stack web developer course accredited by the Univeristy of Birmingham, 
         experienced in HTML, CSS, JavaScript and API Systems
+        </p>
+
+        <p>
+        Technolgies used React, Express, MySQL, Node, Heroku
         </p>
 
         <p>
